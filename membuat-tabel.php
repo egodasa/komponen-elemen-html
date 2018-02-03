b4d68edd31664376016f4d02c630e645e1f28b15
egodasa-mandan <egodasa@users.noreply.github.com>
2018-02-03 23:41:07 +0700

    Membuat tabel generator
    Fitur :
    * Menampilkan data tanpa paginasi dan tombol aksi

---
+++ membuat-tabel.php
@@ -0,0 +1,58 @@
+<?php
+class genTable{
+	private $content;
+	private $header;
+	private $class;
+	private $result;
+	private $id;
+	private $name;
+	public function __construct(){
+		$this->class = new stdClass();
+		$this->class->table = "";
+		$this->class->th = "";
+		$this->class->tr = "";
+		$this->id = "";
+		$this->name = "";
+	}
+	public function setContent($tmp_content){ //set variabel content
+		if(!is_array($tmp_content)) $this->content = [];
+		else $this->content = $tmp_content;
+	}
+	public function setHeader($tmp_header){ //set variabel header
+		if(!is_array($tmp_header)) $this->header = [];
+		else $this->header = $tmp_header;
+	}
+	public function setId($tmp_id){ //set variabel id
+		$this->id = empty($tmp_id) ? "" : " id='".$tmp_id."'";
+	}
+	public function setName($tmp_name){ //set variabel name
+		$this->name = empty($tmp_name) ? "" : " name='".$tmp_name."'";
+	}
+	public function setClass($tmp_class){ //set header
+		$this->class->table = empty($tmp_class->table) ? "" : " class='".$tmp_class->table."'";
+		$this->class->tr = empty($tmp_class->tr) ? "" : " class='".$tmp_class->tr."'";
+		$this->class->th = empty($tmp_class->th) ? "" : " class='".$tmp_class->th."'";
+	}
+	public function makeTable(){ //generate tabel
+		if(empty($this->content)) return "Error ! Isi tabel harus ditentukan -> setContent(array[])";
+		else if(empty($this->header)) return "Error ! Heading tabel harus ditentukan -> setHeader(array[])";
+		else {
+			$this->result = "<table".$this->id.$this->name.$this->class->table."><thead><tr".$this->class->tr.">";
+			foreach($this->header as $h){
+				$this->result .= "<th".$this->class->th.">".$h."</th>";
+				}
+			$this->result .= "</tr></thead>";
+			$this->result .= "<tbody>";
+			foreach($this->content as $tr){
+				$this->result .= "<tr".$this->class->tr.">";
+				foreach($tr as $td){
+					$this->result .= "<td>".$td."</td>";
+					}
+				$this->result .= "</tr>";
+				}
+			$this->result .= "</tbody></table>";
+			return $this->result;
+		}
+	}
+	}
+?>
